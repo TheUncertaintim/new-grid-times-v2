b99567f95313fd00cd05ddf4091fe91482174e5d
TheUncertaintim <yujeng.k@gmail.com>
2025-05-20 20:57:34 +0800

    Complete Exercise 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopStyleWrapper>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopStyleWrapper>
         <Logo />
+        <DesktopStyleWrapper>
+          <Button>Subscribe</Button>
+          <LoginLink>Already a subscriber?</LoginLink>
+        </DesktopStyleWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +83,38 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media (${QUERIES.tabletAndUp}) {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media (${QUERIES.laptopAndUp}) {
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 81px;
+  }
+`;
+
+const DesktopStyleWrapper = styled.div`
+  display: none;
+
+  @media (${QUERIES.laptopAndUp}) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
+
+const LoginLink = styled.a`
+  font-size: ${14 / 16}rem;
+  font-style: italic;
+  font-weight: var(--font-weight-normal);
+  font-family: var(--font-family-serif);
+  color: var(--color-gray-900);
+  text-decoration: underline;
+  text-align: center;
+  margin-top: 8px;
 `;
 
 export default Header;
