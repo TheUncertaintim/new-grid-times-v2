dae8e0d92bc8bca50ea176e6fa9b93fb3f7597d6
TheUncertaintim <yujeng.k@gmail.com>
2025-05-21 22:22:45 +0800

    Handle story borders

--- src/components/OpinionStory/OpinionStory.jsx
+++ src/components/OpinionStory/OpinionStory.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
@@ -17,6 +17,8 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  background-color: var(--color-gray-100);
+  padding: 16px 0;
 `;
 
 const Avatar = styled.img`
